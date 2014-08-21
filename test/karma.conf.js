6762160d59c0a459a51cd479f0e20418d01330e4
maxwellito <@mxwllt>
2014-08-21 19:38:15 +0100

    Add coverage to Karma

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -29,14 +29,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '../src/pathformer.js': ['coverage'],
+      '../src/vivus.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : '../coverage/'
+    },
 
     // web server port
     port: 9876,
