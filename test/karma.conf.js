cbdf85ed319d8b41a557eb42eb35d9d17b6f791d
maxwellito <duluc.maxime@gmail.com>
2019-03-23 14:41:36 +0000

    Make Karma do single run

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   };
 
   if (process.env.TRAVIS) {
